3682b2af99f2e55cee50f886b58462a367b3fdac
alswjd370304-sys <alswjd370304@gmail.com>
2026-08-24 09:02:36 +0900

    헤더 색변경 완성, 2행 진행중

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,15 @@
 const introSlide = new Swiper('.intro_page', {
     direction: 'vertical',
     mousewheel: true,
+    //어떤 이벤트가 발생했을 때 적는 곳
+    on: {
+        slideChange: function () {
+            const header = document.querySelector('header');
+            if (this.activeIndex === 1 || this.activeIndex >= 3) {
+                header.classList.add('black');
+            } else {
+                header.classList.remove('black');
+            }
+        }
+    }
 });
